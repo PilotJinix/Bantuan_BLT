eaf74279d352dd29e1e56c860cd418b9812f427e
pilotjinix <agungsantoso557@gmail.com>
2022-03-19 10:10:14 +0700

    Dashboard

--- app/Http/Controllers/Main_Controller.php
+++ app/Http/Controllers/Main_Controller.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class Main_Controller extends Controller
 {
     public function index(){
-        return view("layout.main");
+        return view("dashboard.index");
     }
 }
