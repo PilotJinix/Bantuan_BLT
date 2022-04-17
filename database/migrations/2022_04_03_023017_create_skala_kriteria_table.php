ceee51138762d65c5c5f0c42b5fdc73c0c3de189
pilotjinix <agungsantoso557@gmail.com>
2022-04-17 17:16:00 +0700

    Master Data

--- database/migrations/2022_04_03_023017_create_skala_kriteria_table.php
+++ database/migrations/2022_04_03_023017_create_skala_kriteria_table.php
@@ -17,7 +17,7 @@ class CreateSkalaKriteriaTable extends Migration
             $table->id();
             $table->string("kriteria_awal")->references("kode_unik")->on("master_kriteria")->onDelete("cascade")->onUpdate("cascade");
             $table->string("kriteria_pembanding")->references("kode_unik")->on("master_kriteria")->onDelete("cascade")->onUpdate("cascade");
-            $table->decimal("nilai_skala");
+            $table->decimal("nilai_skala")->references("id")->on("master_inverse_tfn")->onDelete("cascade")->onUpdate("cascade");
             $table->timestamps();
         });
     }
