902a80af4fc61dc1f91c0de1d24e8677f6d8e8fb
meliatiya24 <55339793+meliatiya24@users.noreply.github.com>
2022-05-21 20:51:08 +0700

    Nambah leftbar

--- resources/views/layout/leftbar.blade.php
+++ resources/views/layout/leftbar.blade.php
@@ -10,10 +10,11 @@
                 <li><a href="{{route("index_user_admin")}}" aria-expanded="false"><i class="nav-icon ti ti-user"></i><span class="nav-title">Kelola User</span></a></li>
                 <li class="nav-static-title">Data Penerima</li>
                 <li><a href="{{route("index_penerima_admin")}}" aria-expanded="false"><i class="nav-icon ti ti-book"></i><span class="nav-title">Data Penerima</span></a></li>
-
                 <li class="nav-static-title">Perhitungan F-AHP</li>
                 <li><a href="{{route("index_skala")}}" aria-expanded="false"><i class="nav-icon ti ti-comment"></i><span class="nav-title">Master Skala</span></a> </li>
-                </li>
+                <li class="nav-static-title">Prioritas Penerima</li>
+                <li><a href="{{route("index_skala")}}" aria-expanded="false"><i class="nav-icon ti ti-comment"></i><span class="nav-title">Informasi Penerima Bantuan</span></a> </li>
+            </li>
                 <li class="sidebar-banner p-4 bg-gradient text-center m-3 d-block rounded">
                     <h5 class="text-white mb-1">Sistem Penunjang Keputusan</h5>
                     <p class="font-13 text-white line-20">Bantuan Langsung Tunai Dana Desa</p>
